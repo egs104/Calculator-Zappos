ef49637c9d94e988d95538a97e7d4bb4466f60bd
Eric Suarez <egs104@gmail.com>
2016-02-11 15:51:15 -0500

    finished application

---
+++ TipCalculator-Zappos-Watch Extension/TipInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  TipInterfaceController.swift
+//  TipCalculator-Zappos
+//
+//  Created by Eric Suarez on 2/11/16.
+//  Copyright © 2016 Eric Suarez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class TipInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
